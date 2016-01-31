b19adf2c7a3cc1b1dcfd54ee96e172bc1707478b
NULL-OPERATOR <x@cryptolab.net>
2016-01-31 14:05:03 +0000

    refactor infinite scroll + make sure duplicate images aren't loaded

--- app/js/services.js
+++ app/js/services.js
@@ -6,21 +6,41 @@ angular.module('imgApp.services', [])
     this.images = [];
     this.busy = false;
     this.after = '';
+    this.idList = []
   };
+
+Flickr.prototype.addID = function(image) {
+  this.idList.push(image.author_id);
+};
+
+Flickr.prototype.addImage = function(image) {
+  this.images.push(image);
+};
+
+Flickr.prototype.hasID = function(image) {
+  return this.idList.includes(image.author_id);
+};
+
+Flickr.prototype.addNewImages = function(images) {
+  for (var i = 0; i < images.length; i++) {
+    var image = images[i]
+    if( this.hasID(image) ) {
+      continue;
+    } else {
+      this.addImage(image);
+      this.addID(image)
+    };
+  }
+};
+
 
 Flickr.prototype.nextPage = function() {
   if (this.busy) return;
   this.busy = true;
-
   var url = "http://api.flickr.com/services/feeds/photos_public.gne?after=" + this.after + "&format=json&jsoncallback=JSON_CALLBACK";
-  console.log(url)
-  $http.jsonp(url).success(function(data) {
-    var images = data.items;
-    for (var i = 0; i < images.length; i++) {
-      console.log(images[i])
-      this.images.push(images[i]);
-    }
 
+  $http.jsonp(url).success(function(data) {
+    this.addNewImages(data.items)
     this.after = "t3_" + this.images[this.images.length - 1].author_id;
     this.busy = false;
   }.bind(this));
